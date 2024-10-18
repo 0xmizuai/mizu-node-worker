dde4f7a0b0dea75422cd8c02a325caf4ac9eee0e
Germano Percossi <germano.percossi@gmail.com>
2024-10-18 02:28:58 +0100

    Introduce pure redis workers
    Instead of getting from redis the data to work on and the function to
    apply, this kind of workers get the just the data to work on and apply
    a specific function. This was they can get data directly from the node.

--- README.md
+++ README.md
@@ -23,16 +23,21 @@ poetry run dev # for dev
 poetry run start # for prod
 ```
 
-#### Start a RQ worker
+#### Start workers
 
-The worker needs additional enironment variables to access R2:
+The worker needs additional environment variables to access R2:
 - AWS_ACCESS_KEY_ID
 - AWS_SECRET_ACCESS_KEY
 - CLOUDFLARE_R2_ID
 - CLOUDFLARE_R2_BUCKET_NAME
+- REDIS_URL
+- REDIS_QUEUE
+
+`REDIS_QUEUE` is needed only by standard worker and not rq_worker. It's an error if not set
 
 ```shell
-poetry run start_worker
+poetry run start_worker  # to start a standard worker
+poetry run start_rq_worker  # to start a worker using rq. This workers require the http server running
 ```
 There is no need to start a worker on the same machine, as long as there is at least
 another worker listening on the same redis server and queue.  
--- mizu_validator/main.py
+++ mizu_validator/main.py
@@ -13,8 +13,9 @@ app = FastAPI()
 # Redis configuration
 redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
 redis_conn = Redis.from_url(redis_url)
-queue_name = "classification_jobs"
-queue = Queue(queue_name, connection=redis_conn)
+rq_queue_name = "classification_jobs"
+redis_queue = os.environ["REDIS_QUEUE"]  # raise exception if not present
+queue = Queue(rq_queue_name, connection=redis_conn)
 
 
 @app.post("/classify")
@@ -32,6 +33,17 @@ def start():
     uvicorn.run("mizu_validator.main:app", host="0.0.0.0", port=8001)
 
 
-def start_worker():
-    worker = Worker([queue_name], connection=redis_conn)
+def start_rq_worker():
+    worker = Worker([rq_queue_name], connection=redis_conn)
     worker.work()
+
+
+def start_worker():
+    p = redis_conn.pubsub()
+    p.subscribe(redis_queue)
+    for message in p.listen():
+        try:
+            worker_job = WorkerJob.parse_obj(message)
+            job_worker(worker_job)
+        except Exception as e:
+            print(e)
